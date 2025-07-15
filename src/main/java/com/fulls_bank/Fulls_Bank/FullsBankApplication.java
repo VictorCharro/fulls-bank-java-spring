6de24d5d3b5bbdf940b859934b68e2ff36d7771c
VcsDevs <v_chs@hotmail.com>
2025-07-15 15:43:51 -0300

    First commit

---
+++ src/main/java/com/fulls_bank/Fulls_Bank/FullsBankApplication.java
@@ -0,0 +1,13 @@
+package com.fulls_bank.Fulls_Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullsBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullsBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fulls_bank/Fulls_Bank/FullsBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fulls_bank.Fulls_Bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullsBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
